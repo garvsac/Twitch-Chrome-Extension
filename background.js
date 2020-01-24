7bdc2784e4c195db3dc935f49f7fcc27561b3e87
garvsac <garv.sachdeva18@gmail.com>
2020-01-25 05:12:43 +0530

    retrived followers

--- background.js
+++ background.js
@@ -6,7 +6,7 @@ clientid = "wkx1l5vtmsi33feoavz1xu0q17861g";
 url = "https://ptsv2.com/t/r8zkh-1579820545/post";
 follows_api = "https://api.twitch.tv/helix/users/follows?from_id=";
 userID_api = "https://api.twitch.tv/helix/users?login=";
-//https://api.twitch.tv/helix/streams?user_login=justcooman&user_login=gorgc'
+streamdata_api = "https://api.twitch.tv/helix/streams?user_login=";
 //fetch( url , {method: "POST", body: JSON.stringify(resp[0])});
 
 function getID(username) {
@@ -21,24 +21,42 @@ function getID(username) {
 async function getFollows(username) {
   let userID = await getID(username);
   let answer =  fetch( follows_api + userID, { headers: { 'client-id': clientid}})
+  //not all follows returned use cursor fix
   .then(resp => resp.json())
-  .then(val => {
-    return val.data;
+  .then(channels => {
+    data = channels.data
+    for ( var i = 0; i < data.length; i++)
+      {
+        data[i] = data[i].to_name
+      }
+    return data;
   })
   return answer;
 }
 
 async function getFollowsStatus(username){
   let channels = await getFollows(username);
-  for ( channel of channels)
+  let answer = [];
+  var i=0;
+  for ( channel of channels )
+  {
+    x =  await fetch( streamdata_api + channel, { headers: { 'client-id': clientid }})
+    .then(resp => resp.json())
+    .then(resp => resp.data);
+    console.log(x)
+    if(x.length>0)
     {
-      console.log(channel)
+      answer[i] = x[0];
+      i++;
     }
-    return channels
+  }
+  return answer;
 }
 //fetch( url , {method: "POST", body: JSON.stringify(getFollows("gorgc"))});
-getFollowsStatus("gorgc")
+getFollowsStatus("shroud")
 .then(resp => {
+  console.log("end")
   console.log(resp);
 
-});
+})
+.catch( err => console.log (err));
