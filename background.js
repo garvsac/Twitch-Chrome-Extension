140f6cdec6d5e8a39414818ecd414cc6d6889269
garvsac <garv.sachdeva18@gmail.com>
2020-01-25 04:19:53 +0530

    change

--- background.js
+++ background.js
@@ -1,20 +1,44 @@
 chrome.runtime.onInstalled.addListener(function() {
   console.log("bg.js")
 });
-//post test
+
+clientid = "wkx1l5vtmsi33feoavz1xu0q17861g";
 url = "https://ptsv2.com/t/r8zkh-1579820545/post";
+follows_api = "https://api.twitch.tv/helix/users/follows?from_id=";
+userID_api = "https://api.twitch.tv/helix/users?login=";
+//https://api.twitch.tv/helix/streams?user_login=justcooman&user_login=gorgc'
+//fetch( url , {method: "POST", body: JSON.stringify(resp[0])});
 
+function getID(username) {
+  let answer =  fetch( userID_api + username, { headers: { 'client-id': clientid}})
+  .then(resp => resp.json())
+  .then(val => {
+    return val.data[0].id;
+  })
+  return answer;
+}
 
-//twitch api
-fetch('https://api.twitch.tv/helix/streams?user_login=justcooman&user_login=gorgc&user_login=masondota2', {
-    headers: {
-        'client-id': 'wkx1l5vtmsi33feoavz1xu0q17861g'
+async function getFollows(username) {
+  let userID = await getID(username);
+  let answer =  fetch( follows_api + userID, { headers: { 'client-id': clientid}})
+  .then(resp => resp.json())
+  .then(val => {
+    return val.data;
+  })
+  return answer;
+}
+
+async function getFollowsStatus(username){
+  let channels = await getFollows(username);
+  for ( channel of channels)
+    {
+      console.log(channel)
     }
-}).then(resp => {
-    return resp.json()
-}).then(resp => {
-    console.log(resp)
-    fetch( url , {method: "POST", body: JSON.stringify(resp)});
-}).catch(err => {
-    console.log(err)
+    return channels
+}
+//fetch( url , {method: "POST", body: JSON.stringify(getFollows("gorgc"))});
+getFollowsStatus("gorgc")
+.then(resp => {
+  console.log(resp);
+
 });
